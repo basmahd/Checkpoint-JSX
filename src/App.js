10594c07770f3f5ed3eca7a2fd950d61a02d6578
unknown <bsm.hamdi09@gmail.com>
2020-11-30 11:21:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import photo from "./imageInPublic.jpg";
+import image from "./imageInSrc.jpg";
+import myvideo from "./myvideo.mp4";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div style={{ border: 'solid 1px black max-width:100vw' }}> 
+        <h1 className="title red">My forum </h1>
+        <br />
+        <img src={image} alt="imageInSrc" />
+        <br />
+        <img src={photo} alt="imageInPublic"/>
+        </div>
+        <video width="320" height="240" controls>
+           <source src={myvideo} type="video/mp4" />
+
+      </video>
     </div>
   );
 }
